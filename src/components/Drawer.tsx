4bb71cb977cc0c8d6bc7d84c099ed90fe3d5396d
Phillip Nunes <phillipnunes86@gmail.com>
2021-09-19 18:36:47 -0300

    Components: Finished drawer layout displaying real data

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -10,16 +10,27 @@ import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import {Avatar, Grid} from "@mui/material";
 
 const drawerWidth = '50%';
 
 function Drawer({ children, posts }: {children: any, posts: []}) {
   const [mobileOpen, setMobileOpen] = useState(false);
-  console.log('UUUUU', posts)
 
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
   };
+
+  const formatDate = (timestamp: number) => {
+    const date = new Date(timestamp * 1000)
+    const now = new Date()
+
+    // @ts-ignore
+    const diff = Math.abs(now - date)
+    const dateInDays = Math.ceil(diff / (1000 * 3600))
+
+    return dateInDays
+  }
 
   const drawer = (
     <div>
@@ -27,15 +38,35 @@ function Drawer({ children, posts }: {children: any, posts: []}) {
       <Divider />
       <List>
         {posts?.map((post: any) => {
-          const { id, title, author, created_utc } = post?.data
+          const { id, title, author, created_utc, thumbnail, url, num_comments } = post?.data
           return (
-            <div key={id}>
-              <ListItem button>
-                <ListItemText primary={title} />
+            <>
+              <ListItem button key={id} style={{
+                flexDirection: 'column',
+              }}>
+                <Grid container xs={12}>
+                  <Grid item md={11} >
+                    <Typography mb={1}>{`Posted by ${author} ${formatDate(created_utc)} hours ago`}</Typography>
+                  </Grid>
+                  <Grid item md={1}>
+                    <Typography mb={1}>Unread</Typography>
+                  </Grid>
+                </Grid>
+                <Grid container xs={12} alignItems='center'>
+                  <Grid item md={2}>
+                    <Avatar src={thumbnail} alt="Post image" sx={{ width: 56, height: 56 }} />
+                  </Grid>
+                  <Grid item md={10}>
+                    <ListItemText primary={title} />
+                  </Grid>
+                </Grid>
+                <Grid container xs={12}>
+                  <Typography mt={1}>{`${num_comments} comments`}</Typography>
+                </Grid>
               </ListItem>
               <Divider />
-            </div>
-        )
+            </>
+          )
         })}
       </List>
     </div>
